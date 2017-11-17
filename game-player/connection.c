eb7ca78a962626dfe6907eb6fdf71fde16a9a84b
Marylou <marylou.gouneau@edu.ece.fr>
2017-11-17 22:37:32 +0100

    Basic function in connection.c about Socket TCP Client

--- game-player/connection.c
+++ game-player/connection.c
@@ -1,1 +1,67 @@
 #include "connection.h"
+
+int createSocket()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        printf("game-player : connection.c in function createSocket() : Could not create socket \n");
+    }
+    puts("Socket created \n");
+    return sock;
+}
+
+int connectSocket(int socket)
+{
+    struct sockaddr_in server;
+    //Define the socket remote address : 127.0.0.1:8888
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
+
+    //Connect to remote server
+    if (connect(socket, (struct sockaddr*)&server, sizeof(server)) < 0)
+    {
+        perror("game-player : connection.c in function connectSocket() : Connect failed. Error \n");
+        return -1;
+    }
+    puts("Connected \n");
+    return 0;
+}
+
+char* readMessage(int socket)
+{
+    char message[2000];
+    if (read(socket, message, 2000) < 0)
+    {
+        puts("game-player : connection.c in function readMessage() : read failed \n");
+        return NULL;
+    }
+
+    int msgLength = message[1];
+    char *shortMessage;
+    shortMessage = (char*) malloc((msgLength+4)*sizeof(char));
+    int i;
+    for(i=0; i<(msgLength+4); i++)
+    {
+        shortMessage[i]=message[i];
+    }
+    return message;
+}
+
+
+int writeMessage(int socket, char *message)
+{
+    if (write(socket, message, strlen(message), 0) < 0)
+    {
+        puts("game-player : connection.c in function writeMessage() : write failed");
+        return -1;
+    }
+    return 0;
+}
+
+
+int disconnect(int socket)
+{
+    close(socket);
+}
--- game-player/connection.h
+++ game-player/connection.h
@@ -1,6 +1,11 @@
 #ifndef CONNECTION_H_INCLUDED
 #define CONNECTION_H_INCLUDED
 
+#include<stdio.h> //printf
+#include<string.h> //strlen
+#include<sys/socket.h> //socket
+#include<arpa/inet.h> //inet_addr
+
 /*
  *
  */
@@ -9,16 +14,21 @@ int createSocket();
 /*
  *
  */
-int connect(int socket);
+int connectSocket(int socket);
 
 /*
  *
  */
-int readMessage(int socket);
+char* readMessage(int socket);
 
 /*
  *
  */
-int writeMessage(int socket);
+int writeMessage(int socket, char *message);
+
+/*
+ *
+ */
+int disconnect(int socket);
 
 #endif // CONNECTION_H_INCLUDED
